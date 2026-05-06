a68a684537b5fa9c838c98dfa54e580601bc9dca
Nand0-23 <ahernandez75@sfsu.edu>
2026-05-05 18:56:47 -0700

    updated routes

--- application/server.js
+++ application/server.js
@@ -16,6 +16,8 @@ const goalRoutes = require('./routes/goalRoutes');
 const wellnessRoutes = require('./routes/wellnessRoutes');
 const academicRoutes = require('./routes/academicRoutes');
 const adminRoutes = require('./routes/adminRoutes');
+const careerRoutes = require('./routes/careerRoutes');
+const resourceRoutes = require('./routes/resourceRoutes');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -79,6 +81,8 @@ app.use('/api/goals', goalRoutes);          // Goals routes
 app.use('/api/wellness', wellnessRoutes);   // Wellness/Mood routes
 app.use('/api/academic', academicRoutes);   // Academic/Study/Task routes
 app.use('/api/admin', adminRoutes);         // Admin routes
+app.use('/api/career', careerRoutes);       // Career page routes
+app.use('/api/resources', resourceRoutes);   // Resource module routes
 
 // Start the server
 app.listen(port, "127.0.0.1", () => {
